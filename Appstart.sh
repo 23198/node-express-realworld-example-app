7add7e1fe56e3048147cc639be21220dab12ede6
23198 <31165700+23198@users.noreply.github.com>
2020-06-25 22:30:34 +0530

    Update Appstart.sh

--- Appstart.sh
+++ Appstart.sh
@@ -8,7 +8,7 @@ sudo apt get install curl -y
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash 
 sudo apt-get install nodejs -y
 sudo npm i -g pm2
-sudo pm2 start ./app.js --env MONGODB_URI
+sudo pm2 start -f ./app.js --env MONGODB_URI
 #sudo rm -rf /etc/nginx/sites-available/default
 #sudo mv default   /etc/nginx/sites-available/
 #sudo systemctl restart nginx
