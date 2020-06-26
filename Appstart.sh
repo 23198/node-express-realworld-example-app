269c44aaa4da21ac407c167cb14eab2a54e8b1a5
23198 <31165700+23198@users.noreply.github.com>
2020-06-26 15:55:46 +0530

    Update Appstart.sh

--- Appstart.sh
+++ Appstart.sh
@@ -11,7 +11,8 @@ sudo apt get install curl -y
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash 
 sudo apt-get install nodejs -y
 sudo npm i -g pm2
-sudo pm2 start -f /apps/app.js 
+sudo pm2 stop all
+sudo pm2 start -f app.js 
 #sudo rm -rf /etc/nginx/sites-available/default
 #sudo mv default   /etc/nginx/sites-available/
 #sudo systemctl restart nginx
