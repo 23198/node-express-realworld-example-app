90c37c99dad22c83d05b6342449ae099c5b87a95
23198 <31165700+23198@users.noreply.github.com>
2020-06-26 12:21:28 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
-export MONGODB_URI="/apps/uri.txt"
+#echo "$(cat /apps/uri.txt )"
+export MONGODB_URI="$(cat /apps/uri.txt )"
+#export MONGODB_URI="/apps/uri.txt"
 echo $MONGODB_URI
+~                   
