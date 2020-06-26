197312b4bf98a5ff52320d5d2090033be995a186
23198 <31165700+23198@users.noreply.github.com>
2020-06-26 11:06:29 +0530

    new update

--- Appstart.sh
+++ Appstart.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-cd /apps
+#cd /apps
 export MONGODB_URI=$( cat /apps/uri.txt )
+echo $MONGODB_URI
+echo "RUNNING"
 # export NODE_ENV=production
 # export SECRET=secret
 sudo apt-get update -y
@@ -8,7 +10,7 @@ sudo apt get install curl -y
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash 
 sudo apt-get install nodejs -y
 sudo npm i -g pm2
-sudo pm2 start -f ./app.js --env MONGODB_URI
+sudo pm2 start -f /apps/app.js --env MONGODB_URI
 #sudo rm -rf /etc/nginx/sites-available/default
 #sudo mv default   /etc/nginx/sites-available/
 #sudo systemctl restart nginx
